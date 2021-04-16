5335b3741ca596d66e47df8c40af6582c9755600
tnordquist <toddnordquist144@gmail.com>
2021-04-16 14:02:50 -0600

    Basic multi-line output works.

--- app/src/main/java/edu/cnm/deepdive/randomnumbers/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/randomnumbers/MainActivity.java
@@ -1,27 +1,52 @@
 package edu.cnm.deepdive.randomnumbers;
 
-import android.app.Activity;
+import static java.lang.Integer.parseInt;
+
+import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import edu.cnm.deepdive.randomnumbers.databinding.ActivityMainBinding;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
   ActivityMainBinding binding;
+  private boolean updateInProgress;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
-  }
+    binding.limit.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-  // TODO set up TextWatcher
-  // TODO set up random number based on edittext input and display value when button is clicked.
+      }
+
+      @Override
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+        if (!updateInProgress) {
+          updateInProgress = true;
+          String cleaned = editable.toString().trim();
+          int value = parseInt(cleaned);
+          Random rng = new Random();
+          StringBuilder nums = new StringBuilder();
+          for (int i = 0; i < 5; i++) {
+            int rngVal = rng.nextInt(value) + 1;
+            nums.append(rngVal).append("\n");
+          }
+          binding.randomNumbers.setText(nums);
+          updateInProgress = false;
+        }
+      }
+    });
+  }
 
 }
