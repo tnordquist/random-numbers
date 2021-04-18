e58ca8581d1084d7d48a163126271c10b950c526
tnordquist <toddnordquist144@gmail.com>
2021-04-17 21:31:43 -0600

    Floating button clicked displays numbers.

--- app/src/main/java/edu/cnm/deepdive/randomnumbers/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/randomnumbers/MainActivity.java
@@ -4,13 +4,8 @@ import static java.lang.Integer.parseInt;
 
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.cnm.deepdive.randomnumbers.databinding.ActivityMainBinding;
@@ -19,7 +14,6 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
   ActivityMainBinding binding;
-  private boolean updateInProgress;
   private int seekvalue = 5;
 
   @RequiresApi(api = VERSION_CODES.O)
@@ -44,45 +38,19 @@ public class MainActivity extends AppCompatActivity {
 
       }
     });
-    binding.numberDisplay.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        seekvalue = binding.bar.getProgress();
+    binding.numberDisplay.setOnClickListener((view) -> {
+      seekvalue = binding.bar.getProgress();
+      @SuppressWarnings("ConstantConditions") String cleaned = binding.limit.getText().toString().trim();
+      int value = parseInt(cleaned);
+      Random rng = new Random();
+      StringBuilder nums = new StringBuilder();
+      for (int i = 0; i < seekvalue; i++) {
+        int rngVal = rng.nextInt(value) + 1;
+        nums.append(rngVal).append("\n");
       }
+      binding.randomNumbers.setText(nums);
     });
     setContentView(binding.getRoot());
-    binding.limit.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-      }
-
-      @Override
-      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-      }
-
-      @Override
-      public void afterTextChanged(Editable editable) {
-        if (!updateInProgress) {
-          updateInProgress = true;
-          String cleaned = editable.toString().trim();
-          int value = parseInt(cleaned);
-          if (value > Integer.MAX_VALUE) {
-            Toast.makeText(MainActivity.this, "Enter a smaller number", Toast.LENGTH_LONG).show();
-          } else {
-            Random rng = new Random();
-            StringBuilder nums = new StringBuilder();
-            for (int i = 0; i < seekvalue; i++) {
-              int rngVal = rng.nextInt(value) + 1;
-              nums.append(rngVal).append("\n");
-            }
-            binding.randomNumbers.setText(nums);
-            updateInProgress = false;
-          }
-        }
-      }
-    });
   }
 
 }
